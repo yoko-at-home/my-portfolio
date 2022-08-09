58e9180fa465dd8a293e480ead4b7733c8c5b586
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-08-09 23:43:56 +0900

    Fixed Header to top with intersection

--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -1,20 +1,34 @@
 /* eslint-disable @next/next/no-img-element */
 import Link from "next/link";
+import { CSSProperties } from "react";
 import { Navigation, NavMobile } from "src/layout/navigation";
 
-export const HeaderPart = () => {
+type Props = {
+  inView: boolean;
+  className?: CSSProperties;
+};
+
+export const HeaderPart = ({ inView }: Props) => {
   return (
-    <div className="flex justify-around py-5 leading-6">
-      <NavMobile />
-      <div className="text-[18px] font-bold">
-        <Link href="/">Shimabu IT University</Link>
-      </div>
-      <div className="flex leading-6">
-        <div className="hidden sm:block">
-          <Navigation />
+    <div className="sticky top-0 z-50">
+      <div
+        className={
+          inView
+            ? "flex justify-around py-5 leading-6"
+            : "top-[0] flex justify-around bg-slate-50/60 py-5 leading-6"
+        }
+      >
+        <NavMobile />
+        <div className="text-[18px] font-bold">
+          <Link href="/">Shimabu IT University</Link>
         </div>
-        <div className="border-1 border-gray-400 pt-1">
-          <img src="/assets/svgs/night.svg" alt="night mode" />
+        <div className="flex leading-6">
+          <div className="hidden sm:block">
+            <Navigation />
+          </div>
+          <div className="border-1 border-gray-400 pt-1">
+            <img src="/assets/svgs/night.svg" alt="night mode" />
+          </div>
         </div>
       </div>
     </div>
--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,16 +1,19 @@
 import { FC, ReactNode } from "react";
 import { FooterPart } from "src/layout/footer";
 import { HeaderPart } from "src/layout/header";
+import { useInView } from "react-intersection-observer";
 
 type Body = {
   children: ReactNode;
+  inView?: boolean;
 };
 
 export const Layout: FC<Body> = (props) => {
+  const { inView, ref } = useInView({ threshold: 0 });
   return (
     <div className="flex min-h-screen flex-col text-[#25262B]">
-      <HeaderPart />
-      <div className="flex flex-col grow justify-center">{props.children}</div>
+      <HeaderPart inView={inView} />
+      <div className="flex grow flex-col justify-start">{props.children}</div>
       <FooterPart />
     </div>
   );
