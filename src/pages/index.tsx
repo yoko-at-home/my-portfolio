be822b9384de242b22207fed0507f403ff6e6cde
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-08-09 14:05:14 +0900

    ⚰️ remove dead code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,10 @@
 import type { NextPage } from "next";
-import { ViewButton } from "src/components/buttons";
 import { Layout } from "src/layout";
-import { Button, useMediaQuery, useViewportSize } from "src/lib/mantine";
 
 const Home: NextPage = () => {
-  const { width } = useViewportSize();
-  const largerThanXs = useMediaQuery("xs");
-  const largerThanSm = useMediaQuery("sm");
-  const largerThanMd = useMediaQuery("md");
-  const largerThanLg = useMediaQuery("lg");
-  const largerThanXl = useMediaQuery("xl");
-
-  const handleClick = () => {
-    console.log("Hello!");
-  };
-
   return (
     <Layout>
-      <div className="p-20">
-        Homeです
-        <Button dent onClick={handleClick} className="mt-4 block">
-          Click me!
-        </Button>
-        <Button onClick={handleClick} className="mt-4 block">
-          Click me!
-        </Button>
-        <ViewButton type={"dark"} onClilck={handleClick}>
-          View All
-        </ViewButton>
-        <ViewButton type={"light"} onClilck={undefined}>
-          Edit
-        </ViewButton>
-      </div>
+      <div className="p-20">Homeです</div>
     </Layout>
   );
 };
