6c384c0106b8a8485a041971db67b72836d110db
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-08-09 23:44:19 +0900

    Added About section content

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,8 +1,19 @@
 import { NextPage } from "next";
 import { Layout } from "src/layout";
+import { Title } from "src/components/title";
 
 const AboutPage: NextPage = () => {
-  return <Layout>About pageです</Layout>;
+  return (
+    <Layout>
+      <div className="mx-auto max-w-sm">
+        <Title>About</Title>
+        <div>Lightsound Shimabu </div>
+        <div>
+          ITエンジニアYouTuber。神戸大学経営学部卒。未経験から独学でプログラミングを勉強し、新卒でヤフーに入社。2019年に株式会社GameHintを創業。
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default AboutPage;
