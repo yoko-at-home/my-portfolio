7acf8d75ac9e1b536b9390751ea84ead0069565c
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-08-09 22:58:16 +0900

    twitter data

--- src/components/twitter/data.ts
+++ src/components/twitter/data.ts
@@ -1,50 +1,29 @@
 export const data = [
   {
     id: "1",
-    thumbnail:"/assets/img/Thumbnail.webp",
+    thumbnail: "/assets/img/Ellipse 2.png",
     title: "IT KINGDOM",
     content:
       "当サロンのLPページ。React、Next.js、TypeScriptなどのモダンな技術を用いて作られています。初心者にちょうど良い難易度の制作物です。",
     date: "2021.10 - 2021.12",
+    account: "yokoiwasaki6",
   },
   {
     id: "2",
-    thumbnail:"/assets/img/Thumbnail.webp",
+    thumbnail: "/assets/img/Ellipse 2.png",
     title: "IT KINGDOM 1",
     content:
       "当サロンのLPページ。React、Next.js、TypeScriptなどのモダンな技術を用いて作られています。初心者にちょうど良い難易度の制作物です。",
     date: "2021.10 - 2021.12",
+    account: "yokoiwasaki6",
   },
   {
     id: "3",
-    thumbnail:"/assets/img/Thumbnail.webp",
+    thumbnail: "/assets/img/Ellipse 2.png",
     title: "IT KINGDOM 2",
     content:
       "当サロンのLPページ。React、Next.js、TypeScriptなどのモダンな技術を用いて作られています。初心者にちょうど良い難易度の制作物です。",
     date: "2021.10 - 2021.12",
-  },
-  {
-    id: "4",
-    thumbnail:"/assets/img/Thumbnail.webp",
-    title: "IT KINGDOM 3",
-    content:
-      "当サロンのLPページ。React、Next.js、TypeScriptなどのモダンな技術を用いて作られています。初心者にちょうど良い難易度の制作物です。",
-    date: "2021.10 - 2021.12",
-  },
-  {
-    id: "5",
-    thumbnail:"/assets/img/Thumbnail.webp",
-    title: "IT KINGDOM 3",
-    content:
-      "当サロンのLPページ。React、Next.js、TypeScriptなどのモダンな技術を用いて作られています。初心者にちょうど良い難易度の制作物です。",
-    date: "2021.10 - 2021.12",
-  },
-  {
-    id: "6",
-    thumbnail:"/assets/img/Thumbnail.webp",
-    title: "IT KINGDOM 3",
-    content:
-      "当サロンのLPページ。React、Next.js、TypeScriptなどのモダンな技術を用いて作られています。初心者にちょうど良い難易度の制作物です。",
-    date: "2021.10 - 2021.12",
+    account: "yokoiwasaki6",
   },
 ];
