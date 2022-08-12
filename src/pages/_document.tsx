7eb8a52b73e047517f617332abe3d25a9c838831
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-08-12 15:39:21 +0900

    Set meta data

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,8 +1,27 @@
-import { createGetInitialProps } from "@mantine/next";
-import Document from "next/document";
+import { NextPage } from "next";
+import { Head, Html, Main, NextScript } from "next/document";
 
-const getInitialProps = createGetInitialProps();
+const MyDocument: NextPage = () => {
+  return (
+    <Html lang="ja">
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="title" content="Lightsound Shimabu" />
+        <meta
+          name="description"
+          content="ITエンジニアYouTuber。神戸大学経営学部卒。未経験から独学でプログラミングを勉強し、新卒でヤフーに入社。2019年に株式会社GameHintを創業。"
+        />
+        <meta
+          property="og:image"
+          content="https://my-portfolio-shimabu.vercel.app/assets/img/Thumbnail.webp"
+        />
+      </Head>
+      <body className="bg-white text-black antialiased">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
 
-export default class _Document extends Document {
-  static getInitialProps = getInitialProps;
-}
+export default MyDocument;
