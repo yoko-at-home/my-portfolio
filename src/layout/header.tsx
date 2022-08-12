41df33045a444be4ce1e6ccedbf55e49f8c50d5e
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-08-12 16:08:55 +0900

    dark mode icon replaced

--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -2,6 +2,7 @@
 import Link from "next/link";
 import { CSSProperties } from "react";
 import { Navigation, NavMobile } from "src/layout/navigation";
+import { Text } from "@mantine/core";
 
 type Props = {
   inView: boolean;
@@ -10,7 +11,7 @@ type Props = {
 
 export const HeaderPart = ({ inView }: Props) => {
   return (
-    <div className="sticky top-0 z-50">
+    <div className="sticky top-0 z-50 items-center">
       <div
         className={
           inView
@@ -19,14 +20,14 @@ export const HeaderPart = ({ inView }: Props) => {
         }
       >
         <NavMobile />
-        <div className="text-[18px] font-bold">
+        <Text size="lg" weight={700}>
           <Link href="/">Shimabu IT University</Link>
-        </div>
+        </Text>
         <div className="flex leading-6">
           <div className="hidden sm:block">
             <Navigation />
           </div>
-          <div className="border-1 border-gray-400 pt-1">
+          <div className="border-1 border-gray-400">
             <img src="/assets/svgs/night.svg" alt="night mode" />
           </div>
         </div>
