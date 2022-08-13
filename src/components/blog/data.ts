3c2bb671cfaf2ea02b862ecaf74d31f4a0a7a7f4
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-08-13 18:04:12 +0900

    🐛

--- src/components/blog/data.ts
+++ src/components/blog/data.ts
@@ -1,70 +1,91 @@
 export const data = [
   {
     id: 1,
-    title: "This is a header",
+    title: "This is a header1",
     content:
       "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.",
     date: "2022.07.11",
   },
   {
     id: 2,
-    title: "This is a header 1",
-    content:
-      "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.",
-    date: "2022.07.11",
-  },
-  {
-    id: 3,
     title: "This is a header 2",
     content:
       "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.",
     date: "2022.07.11",
   },
   {
-    id: 4,
+    id: 3,
     title: "This is a header 3",
     content:
       "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.",
     date: "2022.07.11",
   },
   {
-    id: 5,
+    id: 4,
     title: "This is a header 4",
     content:
       "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.",
     date: "2022.07.11",
   },
   {
-    id: 6,
+    id: 5,
     title: "This is a header 5",
     content:
       "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.",
     date: "2022.07.11",
   },
   {
-    id: 7,
+    id: 6,
     title: "This is a header 6",
     content:
       "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.",
     date: "2022.07.11",
   },
   {
-    id: 8,
+    id: 7,
     title: "This is a header 7",
     content:
       "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.",
     date: "2022.07.11",
   },
   {
-    id: 9,
+    id: 8,
     title: "This is a header 8",
     content:
       "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.",
     date: "2022.07.11",
   },
   {
-    id: 10,
+    id: 9,
     title: "This is a header 9",
+    content:
+      "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.",
+    date: "2022.07.11",
+  },
+  {
+    id: 10,
+    title: "This is a header 10",
+    content:
+      "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.",
+    date: "2022.07.11",
+  },
+  {
+    id: 11,
+    title: "This is a header 11",
+    content:
+      "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.",
+    date: "2022.07.11",
+  },
+  {
+    id: 12,
+    title: "This is a header 12",
+    content:
+      "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.",
+    date: "2022.07.11",
+  },
+  {
+    id: 13,
+    title: "This is a header 13",
     content:
       "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.",
     date: "2022.07.11",
