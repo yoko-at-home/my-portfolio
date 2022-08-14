3da3096ff944fbe58794c67550ca99b79754ee2a
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-08-14 15:25:19 +0900

    loading

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -5,10 +5,12 @@ import { NextPage } from "next";
 import { Layout } from "src/layout";
 import { Image, Pagination } from "@mantine/core";
 import { usePagination } from "@mantine/hooks";
+import { useState } from "react";
 
 const reversedData = data.reverse();
 
 const BlogPage: NextPage = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const pagination = usePagination({ total: 10, initialPage: 1 });
   console.log(pagination);
 
@@ -16,25 +18,30 @@ const BlogPage: NextPage = () => {
     <Layout>
       <div className="mx-4 pb-10 sm:mx-auto">
         <Title>Blog</Title>
-        {reversedData.map((item) => {
-          return (
-            <CardPortion
-              key={item.id}
-              id={item.id}
-              title={item.title}
-              content={item.content}
-              date={item.date}
-            ></CardPortion>
-          );
-        })}
-        <div className="my-16 flex justify-center">
-          <Image
-            src="/assets/svgs/Loader.svg"
-            alt="loader"
-            width={30}
-            height={30}
-          />
-        </div>
+        {isLoading ? (
+          <div className="my-16 flex min-h-fit justify-center">
+            <Image
+              src="/assets/svgs/Loader.svg"
+              alt="loader"
+              width={30}
+              height={30}
+            />
+          </div>
+        ) : (
+          <>
+            {reversedData.map((item) => {
+              return (
+                <CardPortion
+                  key={item.id}
+                  id={item.id}
+                  title={item.title}
+                  content={item.content}
+                  date={item.date}
+                ></CardPortion>
+              );
+            })}
+          </>
+        )}
       </div>
       <div className="mb-16 flex justify-center">
         <Pagination total={data.indexOf.length} color="dark" withEdges />
