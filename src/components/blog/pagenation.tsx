42906bfc5a7210590947cfe93f94c4dcf7a15224
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-08-15 17:23:19 +0900

    create pagination

--- src/components/blog/pagenation.tsx
+++ src/components/blog/pagenation.tsx
@@ -1,27 +1,37 @@
-import { Pagination } from "@mantine/core";
+import { Button, Pagination } from "@mantine/core";
+import Link from "next/link";
 import { FC } from "react";
 import { data } from "src/components/blog/data";
 
 type pagenationProps = {
-  onClick?: () => void;
+  totalCount?: number;
 };
 
-export const PagenationComponent: FC<pagenationProps> = () => {
-  const pagenationTotal = Math.ceil(data.length / 10);
-  const handleOnClick = (e: any) => {
-    e?.preventDefault();
-    alert("clicked");
+export const PagenationComponent: FC<pagenationProps> = (props) => {
+  const totalCount = data.length;
+  const PER_PAGE = 10;
+  const range = (start: number, end: number) => {
+    return [...Array(end - start + 1)].map((_, i) => {
+      return start + i;
+    });
   };
 
   return (
     <div>
-      <Pagination
-        total={pagenationTotal}
-        color="dark"
-        withEdges
-        initialPage={1}
-        onClick={handleOnClick}
-      />
+      {range(1, Math.ceil(totalCount / totalCount)).map((number, index) => {
+        return (
+          <Link href={`/news/page/${number}`} key={index}>
+            <a>
+              <Pagination
+                total={Math.ceil(totalCount / PER_PAGE)}
+                boundaries={1}
+                initialPage={1}
+                color={"dark"}
+              />
+            </a>
+          </Link>
+        );
+      })}
     </div>
   );
 };
