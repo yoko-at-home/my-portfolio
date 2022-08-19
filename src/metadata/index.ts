2b3b88c11146a880bb1198368ccd9d4bc9c597bb
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-08-20 08:20:02 +0900

    fix

--- src/metadata/index.ts
+++ src/metadata/index.ts
@@ -1,7 +1,7 @@
 export const metaData = {
   name: "Shimabu IT University",
   description:
-    "ITエンジニアYouTuber。神戸大学経営学部卒。未経験から独学でプログラミングを勉強し、新卒でtwヤフーに入社。2019年に株式会社GameHintを創業。",
+    "ITエンジニアYouTuber。神戸大学経営学部卒。未経験から独学でプログラミングを勉強し、新卒でヤフーに入社。2019年に株式会社GameHintを創業。",
   twitterName: "しまぶーIT大学",
   twitterAccount: "yokoiwasaki6",
   twitterCard: "/statics/img/Thumbnail.webp",
