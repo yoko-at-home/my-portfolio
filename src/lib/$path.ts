c45522af04d95956db5d2cbe73b483e233767886
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-08-23 23:41:56 +0900

    path update

--- src/lib/$path.ts
+++ src/lib/$path.ts
@@ -1,31 +1,74 @@
 export const pagesPath = {
-  "about": {
-    $url: (url?: { hash?: string }) => ({ pathname: '/about' as const, hash: url?.hash })
-  },
-  "blog": {
-    _id: (id: string | number) => ({
-      $url: (url?: { hash?: string }) => ({ pathname: '/blog/[id]' as const, query: { id }, hash: url?.hash })
+  about: {
+    $url: (url?: { hash?: string }) => ({
+      pathname: "/about" as const,
+      hash: url?.hash,
     }),
-    "page": {
+  },
+  blog: {
+    _id: (id: string | number) => ({
+      $url: (url?: { hash?: string }) => ({
+        pathname: "/blog/[id]" as const,
+        query: { id },
+        hash: url?.hash,
+      }),
+    }),
+    page: {
       _id: (id: string | number) => ({
-        $url: (url?: { hash?: string }) => ({ pathname: '/blog/page/[id]' as const, query: { id }, hash: url?.hash })
-      })
+        $url: (url?: { hash?: string }) => ({
+          pathname: "/blog/page/[id]" as const,
+          query: { id },
+          hash: url?.hash,
+        }),
+      }),
     },
-    $url: (url?: { hash?: string }) => ({ pathname: '/blog' as const, hash: url?.hash })
-  },
-  "contact": {
-    $url: (url?: { hash?: string }) => ({ pathname: '/contact' as const, hash: url?.hash })
-  },
-  "portfolio": {
-    _id: (id: string | number) => ({
-      $url: (url?: { hash?: string }) => ({ pathname: '/portfolio/[id]' as const, query: { id }, hash: url?.hash })
+    $url: (url?: { hash?: string }) => ({
+      pathname: "/blog" as const,
+      hash: url?.hash,
     }),
-    $url: (url?: { hash?: string }) => ({ pathname: '/portfolio' as const, hash: url?.hash })
   },
-  "success": {
-    $url: (url?: { hash?: string }) => ({ pathname: '/success' as const, hash: url?.hash })
+  blog2: {
+    _id: (id: string | number) => ({
+      $url: (url?: { hash?: string }) => ({
+        pathname: "/blog/[id]" as const,
+        query: { id },
+        hash: url?.hash,
+      }),
+    }),
+    $url: (url?: { hash?: string }) => ({
+      pathname: "/blog2" as const,
+      hash: url?.hash,
+    }),
   },
-  $url: (url?: { hash?: string }) => ({ pathname: '/' as const, hash: url?.hash })
-}
+  contact: {
+    $url: (url?: { hash?: string }) => ({
+      pathname: "/contact" as const,
+      hash: url?.hash,
+    }),
+  },
+  portfolio: {
+    _id: (id: string | number) => ({
+      $url: (url?: { hash?: string }) => ({
+        pathname: "/portfolio/[id]" as const,
+        query: { id },
+        hash: url?.hash,
+      }),
+    }),
+    $url: (url?: { hash?: string }) => ({
+      pathname: "/portfolio" as const,
+      hash: url?.hash,
+    }),
+  },
+  success: {
+    $url: (url?: { hash?: string }) => ({
+      pathname: "/success" as const,
+      hash: url?.hash,
+    }),
+  },
+  $url: (url?: { hash?: string }) => ({
+    pathname: "/" as const,
+    hash: url?.hash,
+  }),
+};
 
 export type PagesPath = typeof pagesPath
