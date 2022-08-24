c230433bbaa935277646ad65717c1a86e6f88dee
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-08-24 20:30:57 +0900

    types added

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,8 +1,8 @@
 import { MicroCMSListResponse } from "microcms-js-sdk";
 
 export type Blog = {
-  title: string;
-  lead: string;
+  title?: string;
+  lead?: string;
   content?: string;
   createdAt?: string;
   eyecatch?: {
@@ -18,5 +18,25 @@ export type Blog = {
   offset?: number;
   totalCount?: number;
 };
+export type BlogPaginationProps = [
+  blog: {
+    title?: string;
+    lead?: string;
+    content?: string;
+    createdAt?: string;
+    eyecatch?: {
+      url?: string;
+      height?: number;
+      width?: number;
+    };
+    id?: string;
+    publishedAt?: Date;
+    revisedAt?: Date;
+    updatedAt?: Date;
+    limit?: number;
+    offset?: number;
+    totalCount?: number;
+  }
+];
 
 export type BlogProps = MicroCMSListResponse<Blog>;
