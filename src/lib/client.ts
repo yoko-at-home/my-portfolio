dbe0295c4331d0cfd7ae16339bb56eb9fd0ce3bf
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-08-25 10:29:40 +0900

    file extension renamed to ts

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -2,5 +2,5 @@ import { createClient } from "microcms-js-sdk";
 
 export const client = createClient({
   serviceDomain: "yoko-portfolio",
-  apiKey: process.env.API_KEY,
+  apiKey: process.env.API_KEY!,
 });
