2283b8ac60fe385dae11a983edc7c7410240fb0b
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-08-28 14:56:34 +0900

    Remove dead code

--- src/types/types.ts
+++ src/types/types.ts
@@ -12,41 +12,9 @@ export type Blog = {
   };
   id?: string;
   publishedAt?: Date;
-  revisedAt?: Date;
-  updatedAt?: Date;
   limit?: number;
   offset?: number;
   totalCount?: number;
 };
 
-export type BlogPaginationProps = [
-  blog: {
-    id: string;
-    createdAt: Date;
-    title: string;
-    content: HTMLAnchorElement;
-    eyecatch: {
-      url: string;
-      height: number;
-      width: number;
-    };
-    category: {
-      id: string;
-      createdAt: Date;
-      name: string;
-    };
-    lead: string;
-    limit?: number;
-    offset?: number;
-    totalCount?: number;
-  }
-];
-
 export type BlogProps = MicroCMSListResponse<Blog>;
-export type PropsPagination = MicroCMSListResponse<BlogPaginationProps>[];
-
-export type ContactFormInput = {
-  email: string;
-  name: string;
-  message: string;
-};
