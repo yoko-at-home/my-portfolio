3f73ab6fa59a2a9b683e0598637b27a67c76259c
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-09-05 20:02:16 +0900

    update

--- src/layout/footer.tsx
+++ src/layout/footer.tsx
@@ -1,7 +1,9 @@
+import { metaData } from "src/metadata";
+
 export const FooterPart = () => {
   return (
     <div className="mb-14 flex justify-center text-sm text-[#868E96]">
-      {`© ${new Date().getFullYear()}`} Shimabu IT University
+      {`© ${new Date().getFullYear()}`} {metaData.title}
     </div>
   );
 };
