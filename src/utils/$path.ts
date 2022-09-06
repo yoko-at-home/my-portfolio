1cfbcda0d513c3bfa06066a4d98b67c3be8af5dc
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-09-06 20:54:36 +0900

    fix linter error

--- src/utils/$path.ts
+++ src/utils/$path.ts
@@ -1,4 +1,24 @@
 export const pagesPath = {
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+  "$404": {
+    $url: (url?: { hash?: string }) => ({ pathname: '/404' as const, hash: url?.hash })
+  },
+  "about": {
+    $url: (url?: { hash?: string }) => ({ pathname: '/about' as const, hash: url?.hash })
+  },
+=======
+  "about": {
+    $url: (url?: { hash?: string }) => ({ pathname: '/about' as const, hash: url?.hash })
+  },
+>>>>>>> 42f3a21 (fix linter error)
+  "blog": {
+    _id: (id: string | number) => ({
+      $url: (url?: { hash?: string }) => ({ pathname: '/blog/[id]' as const, query: { id }, hash: url?.hash })
+=======
+>>>>>>> 02eed8e (fix linter error)
   $url: (url?: { hash?: string }) => ({
     hash: url?.hash,
     pathname: "/" as const,
@@ -8,6 +28,10 @@ export const pagesPath = {
     $url: (url?: { hash?: string }) => ({
       hash: url?.hash,
       pathname: "/about" as const,
+<<<<<<< HEAD
+=======
+>>>>>>> b37a0a2 (fix linter error)
+>>>>>>> 02eed8e (fix linter error)
     }),
   },
   blog: {
