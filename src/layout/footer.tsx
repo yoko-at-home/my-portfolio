ae66cda9cee93d88d2cd8f46d645dcaf59098de9
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-09-06 15:13:01 +0900

    💄 fix footer mt

--- src/layout/footer.tsx
+++ src/layout/footer.tsx
@@ -2,7 +2,7 @@ import { metaData } from "src/metadata";
 
 export const FooterPart = () => {
   return (
-    <div className="mb-14 flex justify-center text-sm text-[#868E96]">
+    <div className="my-14 flex justify-center text-sm text-[#868E96]">
       {`© ${new Date().getFullYear()}`} {metaData.title}
     </div>
   );
