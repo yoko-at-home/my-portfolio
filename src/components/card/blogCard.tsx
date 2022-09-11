fe2a1b3f570b55679d144201518b964a5160173a
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-09-11 15:45:42 +0900

    🎨 improve structure of code

--- src/components/card/blogCard.tsx
+++ src/components/card/blogCard.tsx
@@ -5,18 +5,16 @@ import { Blog } from "src/types";
 
 export const BlogCard: FC<Blog> = (props) => {
   return (
-    <li key={props.id} className="mb-5 list-none hover:bg-gray-100">
-      <Link href={`/blog/${props.id}`}>
-        <a>
-          <div className="text-lg font-semibold">{props.title}</div>
-          <div className="my-1 overflow-hidden text-ellipsis text-sm line-clamp-2">
-            {props.lead}
-          </div>
-          <div className="pt-2 text-xs">
-            {dayjs(props.publishedAt).format("YYYY年MM月DD日")}
-          </div>
-        </a>
-      </Link>
-    </li>
+    <Link href={`/blog/${props.id}`} passHref key={props.id}>
+      <li className="mb-5 list-none hover:bg-gray-100">
+        <div className="text-lg font-semibold">{props.title}</div>
+        <div className="my-1 overflow-hidden text-ellipsis text-sm line-clamp-2">
+          {props.lead}
+        </div>
+        <div className="pt-2 text-xs">
+          {dayjs(props.publishedAt).format("YYYY年MM月DD日")}
+        </div>
+      </li>
+    </Link>
   );
 };
--- src/components/card/portfolioCard.tsx
+++ src/components/card/portfolioCard.tsx
@@ -4,13 +4,12 @@ import { Blog } from "src/types";
 
 export const PortfolioCard: FC<Blog> = (props) => {
   return (
-    <Link href={`/portfolio/${props.id}`}>
+    <Link href={`/portfolio/${props.id}`} passHref key={props.id}>
       <a>
         <li
           className="relative mx-auto flex h-72 max-w-lg list-none flex-col bg-cover bg-center text-right"
           style={{
-            backgroundImage: `linear-gradient(to right, rgba(0,0,0,0.2), rgba(100,100,333,0.2)),
-  url('${props.eyecatch?.url}')`,
+            backgroundImage: `linear-gradient(to right, rgba(0,0,0,0.2), rgba(100,100,333,0.2)), url(${props.eyecatch?.url})`,
           }}
         >
           <div className="absolute bottom-0 right-0 w-full bg-black/40 p-3 text-white">
