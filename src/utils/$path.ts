ed5edd2748565f7e5cedc4c855df5ffcc9e4c6e2
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-09-11 15:44:05 +0900

    update

--- src/utils/$path.ts
+++ src/utils/$path.ts
@@ -1,4 +1,7 @@
 export const pagesPath = {
+  "$404": {
+    $url: (url?: { hash?: string }) => ({ pathname: '/404' as const, hash: url?.hash })
+  },
   "about": {
     $url: (url?: { hash?: string }) => ({ pathname: '/about' as const, hash: url?.hash })
   },
