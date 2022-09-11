d6f40455b88bddc237ab17b3a8554502d380809f
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-09-11 16:30:14 +0900

    🐛 portfolio image rendered

--- src/components/card/portfolioCards.tsx
+++ src/components/card/portfolioCards.tsx
@@ -8,12 +8,10 @@ export const PortfolioCards = ({
   items: MicroCMSListResponse<Blog>["contents"];
 }) => {
   return (
-    <div className="relative w-screen max-w-7xl">
-      <ul className="grid grid-cols-1 gap-3 sm:grid-cols-2 xl:grid-cols-3">
-        {items.map((item) => {
-          return <PortfolioCard {...item} key={item.id} />;
-        })}
-      </ul>
-    </div>
+    <ul className="grid grid-cols-1 justify-center gap-3 md:grid-cols-2 xl:grid-cols-3">
+      {items.map((item) => {
+        return <PortfolioCard {...item} key={item.id} />;
+      })}
+    </ul>
   );
 };
