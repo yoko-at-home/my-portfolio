9a9f996cd9d8f6197277822393662a043d8ccd3b
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-09-12 16:30:57 +0900

    💄

--- src/components/card/blogCard.tsx
+++ src/components/card/blogCard.tsx
@@ -6,12 +6,12 @@ import { Blog } from "src/types";
 export const BlogCard: FC<Blog> = (props) => {
   return (
     <Link href={`/blog/${props.id}`} passHref key={props.id}>
-      <li className="mb-5 list-none rounded from-[#E3FF45]/30 to-[#8D3EAD]/40 p-3 shadow-lg shadow-slate-200 hover:bg-gradient-to-r">
-        <div className="text-lg font-semibold">{props.title}</div>
+      <li className="mb-8 list-none rounded from-[#E3FF45]/30 to-[#8D3EAD]/40 p-3 shadow-lg shadow-slate-100 hover:bg-gradient-to-r">
+        <div className="mb-3 text-lg font-semibold">{props.title}</div>
         <div className="my-1 overflow-hidden text-ellipsis text-sm line-clamp-2">
           {props.lead}
         </div>
-        <div className="pt-2 text-xs">
+        <div className="pt-2 text-right text-xs">
           {dayjs(props.publishedAt).format("YYYY年MM月DD日")}
         </div>
       </li>
