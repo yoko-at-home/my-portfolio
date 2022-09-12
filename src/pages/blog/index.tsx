10470e06e5c39c545840a8695f545184255f5743
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-09-12 16:31:19 +0900

    blog: introduced infinite scroll

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,46 +1,64 @@
-import { Center, Pagination } from "@mantine/core";
+import { Center, Loader } from "@mantine/core";
+import axios from "axios";
 import type { GetStaticProps, NextPage } from "next";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 import { AppTitle } from "src/components/atom/appTitle";
 import { Title } from "src/components/atom/title";
-import { BlogCard } from "src/components/card";
+import { BlogCards } from "src/components/card/blogCards";
 import { Layout } from "src/layout";
 import { clientBlog } from "src/pages/api/blog";
 import { Blog, BlogProps } from "src/types";
 
 const Blog: NextPage<BlogProps> = (props) => {
-  const handleOnChange = () => {
-    alert("clicked");
-  };
+  const [items, setItems] = useState<BlogProps["contents"]>(props.contents);
 
+  const hasMore =
+    props.totalCount > items.length || props.totalCount !== items.length;
+
+  const fetchBlog = async () => {
+    const { data } = await axios.get<BlogProps>("/api/blog", {
+      params: {
+        offset: items.length,
+      },
+    });
+
+    setItems([...items, ...data.contents]);
+  };
+  const router = useRouter();
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
   return (
     <Layout>
       <AppTitle title="blog" description="ブログ一覧" />
       <div className="flex flex-col justify-between">
         <div className="py-10 sm:mx-auto">
           <Title>Blog</Title>
-          <ul className="my-16 flex flex-col justify-center">
-            {props.contents.map((content) => {
-              return (
-                <BlogCard
-                  id={content.id}
-                  key={content.id}
-                  title={content.title}
-                  createdAt={content.createdAt}
-                  lead={content.lead}
-                />
-              );
-            })}
-          </ul>
+          <InfiniteScroll
+            next={fetchBlog}
+            loader={
+              <Center>
+                <Loader />
+              </Center>
+            }
+            dataLength={items.length}
+            hasMore={hasMore}
+          >
+            <ul className="my-16 flex flex-col justify-center">
+              <BlogCards items={items} />
+            </ul>
+          </InfiniteScroll>
 
-          {props.contents.length > 6 ? (
+          {/* {props.contents.length > 6 ? (
             <Center mt="xl">
               <Pagination
-                onChange={handleOnChange}
                 color={"dark"}
                 total={Math.ceil(props.totalCount / 6) || 1}
               />
             </Center>
-          ) : null}
+          ) : null} */}
         </div>
       </div>
     </Layout>
@@ -48,7 +66,10 @@ const Blog: NextPage<BlogProps> = (props) => {
 };
 
 export const getStaticProps: GetStaticProps<BlogProps> = async () => {
-  const blogData = await clientBlog.getList<Blog>({ endpoint: "blog" });
+  const blogData = await clientBlog.getList<Blog>({
+    endpoint: "blog",
+    queries: { limit: 999 },
+  });
   return {
     props: blogData,
   };
