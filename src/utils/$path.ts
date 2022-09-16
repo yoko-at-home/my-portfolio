95c328701daef3efed6fe9bc6221739465975e10
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-09-17 08:44:36 +0900

    fix linter errors

--- src/utils/$path.ts
+++ src/utils/$path.ts
@@ -1,26 +1,29 @@
 export const pagesPath = {
-  "$404": {
-    $url: (url?: { hash?: string }) => ({ pathname: '/404' as const, hash: url?.hash })
+  $404: {
+    $url: (url?: { hash?: string }) => ({
+      hash: url?.hash,
+      pathname: "/404" as const,
+    }),
   },
+  $url: (url?: { hash?: string }) => ({ hash: url?.hash, pathname: '/' as const }),
   "about": {
-    $url: (url?: { hash?: string }) => ({ pathname: '/about' as const, hash: url?.hash })
+    $url: (url?: { hash?: string }) => ({ hash: url?.hash, pathname: '/about' as const })
   },
   "blog": {
+    $url: (url?: { hash?: string }) => ({ hash: url?.hash, pathname: '/blog' as const }),
     _id: (id: string | number) => ({
-      $url: (url?: { hash?: string }) => ({ pathname: '/blog/[id]' as const, query: { id }, hash: url?.hash })
-    }),
-    $url: (url?: { hash?: string }) => ({ pathname: '/blog' as const, hash: url?.hash })
+      $url: (url?: { hash?: string }) => ({ hash: url?.hash, pathname: '/blog/[id]' as const, query: { id } })
+    })
   },
   "contact": {
-    $url: (url?: { hash?: string }) => ({ pathname: '/contact' as const, hash: url?.hash })
+    $url: (url?: { hash?: string }) => ({ hash: url?.hash, pathname: '/contact' as const })
   },
   "portfolio": {
+    $url: (url?: { hash?: string }) => ({ hash: url?.hash, pathname: '/portfolio' as const }),
     _id: (id: string | number) => ({
-      $url: (url?: { hash?: string }) => ({ pathname: '/portfolio/[id]' as const, query: { id }, hash: url?.hash })
-    }),
-    $url: (url?: { hash?: string }) => ({ pathname: '/portfolio' as const, hash: url?.hash })
-  },
-  $url: (url?: { hash?: string }) => ({ pathname: '/' as const, hash: url?.hash })
+      $url: (url?: { hash?: string }) => ({ hash: url?.hash, pathname: '/portfolio/[id]' as const, query: { id } })
+    })
+  }
 }
 
 export type PagesPath = typeof pagesPath
