b4e7f6f1d5d48714ffa4be30ad82c66a07e5b275
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-09-17 23:24:57 +0900

    SEO update app title

--- src/components/atom/appTitle/index.tsx
+++ src/components/atom/appTitle/index.tsx
@@ -23,6 +23,15 @@ export const AppTitle: FC<Props> = ({ title, description, url }) => {
       <meta property="og:description" content={description} />
       <meta property="og:title" content={title} />
       <meta property="og:image" content={url} />
+      {/* <!-- Twitter --> */}
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content={url} />
+      <meta property="twitter:title" content={metaData.title} />
+      <meta property="twitter:description" content={description} />
+      <meta property="twitter:image" content={url} />
+      <meta name="twitter:image:width" content="1200" />
+      <meta name="twitter:image:height" content="630" />
+      <meta name="author" content="Yoko Iwasaki"></meta>
     </Head>
   );
 };
