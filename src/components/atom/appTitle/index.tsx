7383dd5c573b787ab6f4be30c476b443b13954e7
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-09-18 21:47:44 +0900

    fix?

--- src/components/atom/appTitle/index.tsx
+++ src/components/atom/appTitle/index.tsx
@@ -18,7 +18,7 @@ export const AppTitle: FC<Props> = ({ title, description, url }) => {
       <title>{newTitle}</title>
       <meta name="description" content={description} />
       <link rel="icon" href="/assets/favicons/favicon.ico" />
-      <meta property="og:url" content={`${metaData.siteUrl}${router.asPath}`} />
+      <meta property="og:url" content={metaData.siteUrl + router.asPath} />
       <meta property="og:site_name" content={metaData.title} />
       <meta property="og:description" content={description} />
       <meta property="og:title" content={title} />
