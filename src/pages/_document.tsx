ba5487968cf045824ec520d2a130dca8028b0649
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-09-18 22:09:34 +0900

    fix

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,12 @@
 import { NextPage } from "next";
-import { Html, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from "next/document";
 
 const MyDocument: NextPage = () => {
   return (
     <Html lang="ja">
+      <Head>
+        <meta charSet="utf-8" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
