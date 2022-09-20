e12cf6f33ccb48fbf75075607c6cea72427ee3ed
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-09-20 22:35:51 +0900

    ♻️ refactor type

---
+++ src/types/gitHub.ts
@@ -0,0 +1,10 @@
+export type GitHubCardProps = {
+  id: string;
+  name: string;
+  description: string;
+  forkCount: number;
+  language: string;
+  pinnedItems?: any;
+  stargazerCount: number;
+  url: string;
+};
---
+++ src/types/twitter.ts
@@ -0,0 +1,12 @@
+export type TwitterUserProps = {
+  id: string;
+  name: string;
+  profile_image_url: string;
+  username: string;
+};
+
+export type TwitterTweetProps = {
+  id: string;
+  created_at: string;
+  text: string;
+};
